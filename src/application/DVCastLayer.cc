e910df6540697755cffc4c832bf74964720576ae
Valentine Nwachukwu <valdiz777@gmail.com>
2017-04-12 03:23:50 -0500

    added license information

--- src/application/DVCastLayer.cc
+++ src/application/DVCastLayer.cc
@@ -1,22 +1,26 @@
-//
-// Copyright (C) 2006-2011 Christoph Sommer <christoph.sommer@uibk.ac.at>
-//
-// Documentation for these modules is at http://veins.car2x.org/
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
+/*
+MIT License
+
+Copyright (c) 2017 Valentine Nwachukwu <valdiz777@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 #include "DVCastLayer.h"
 
@@ -80,14 +84,14 @@ void DVCastLayer::handleLowerMsg(cMessage* msg) {
 void DVCastLayer::onData(WaveShortMessage* wsm) {
     EV << "****onData" << endl;
 
+    // ignore messages that I sent out
+    if (!sentMessages.empty() || contains(&sentMessages, wsm->getSerial())) {
+        return;
+    }
+
     if (rcvdMessages.empty() || !contains(&rcvdMessages, wsm->getSerial())) {
         // this is a new message, add to received message queue
         rcvdMessages.push_back(wsm->getSerial());
-        findHost()->getDisplayString().updateWith("r=16,green");
-        annotations->scheduleErase(1,
-                annotations->drawLine(wsm->getSenderPos(),
-                        mobility->getPositionAt(simTime()), "blue"));
-
         bool ODC = (NB_OPPOSITE.size() > 0) ? true : false;
         bool Dflg =
                 (wsm->getRecipientAddress() == getParentModule()->getIndex()) ?
@@ -100,12 +104,25 @@ void DVCastLayer::onData(WaveShortMessage* wsm) {
             // no broadcast suppression
             if (ODC) {
                 sendMessage(wsm->getWsmData(), -1, wsm->getSerial());
+                sentMessages.push_back(wsm->getSerial());
                 if (!Dflg) {
                     findHost()->getDisplayString().updateWith("r=16,blue");
+                    annotations->scheduleErase(1,
+                            annotations->drawLine(wsm->getSenderPos(),
+                                    mobility->getPositionAt(simTime()),
+                                    "blue"));
                 }
             } else {
                 findHost()->getDisplayString().updateWith("r=16,pink");
+                annotations->scheduleErase(1,
+                        annotations->drawLine(wsm->getSenderPos(),
+                                mobility->getPositionAt(simTime()), "blue"));
             }
+        } else {
+            findHost()->getDisplayString().updateWith("r=16,green");
+            annotations->scheduleErase(1,
+                    annotations->drawLine(wsm->getSenderPos(),
+                            mobility->getPositionAt(simTime()), "blue"));
         }
     }
 }
@@ -170,7 +187,9 @@ void DVCastLayer::handlePositionUpdate(cObject* obj) {
         if (simTime() - lastDroveAt >= 10) {
             findHost()->getDisplayString().updateWith("r=16,red");
             if (!sentAccidentMessage) {
-                sendMessage(mobility->getRoadId(), -1, rand() % 101);
+                int serial = rand() % 101;
+                sendMessage(mobility->getRoadId(), -1, serial);
+                sentMessages.push_back(serial);
                 sentAccidentMessage = true;
             }
         }
--- src/application/DVCastLayer.h
+++ src/application/DVCastLayer.h
@@ -1,22 +1,26 @@
-//
-// Copyright (C) 2006-2011 Christoph Sommer <christoph.sommer@uibk.ac.at>
-//
-// Documentation for these modules is at http://veins.car2x.org/
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
+/*
+MIT License
+
+Copyright (c) 2017 Valentine Nwachukwu <valdiz777@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 #ifndef DVCastLayer_H
 #define DVCastLayer_H
@@ -51,6 +55,7 @@ protected:
     double dlastx = 0;
     std::deque<int> NB_FRONT, NB_BACK, NB_OPPOSITE;
     std::deque<int> rcvdMessages;
+    std::deque<int> sentMessages;
     std::map<int, WaveShortMessage*> delayedRB;
     bool sentAccidentMessage;
     bool isParking;
